0eeff2280fac414522ab929e0e364fb54c5225be
Hugo Fabian Alaniz <hfaalaniz@hotmail.com>
2024-10-15 00:15:49 -0300

    Add files via upload

---
+++ Tarjeta.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Sistema
+{
+    partial class Tarjeta
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTitulo = new System.Windows.Forms.Panel();
+            this.picTitulo = new Sistema.PctureBox();
+            this.lblContenido = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pnlSeparador = new System.Windows.Forms.Panel();
+            this.pnlCuerpo = new System.Windows.Forms.Panel();
+            this.lblLinea3 = new System.Windows.Forms.Label();
+            this.lblLinea2 = new System.Windows.Forms.Label();
+            this.lblLinea1 = new System.Windows.Forms.Label();
+            this.picCuerpo = new System.Windows.Forms.PictureBox();
+            this.pnlTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTitulo)).BeginInit();
+            this.pnlCuerpo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCuerpo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTitulo
+            // 
+            this.pnlTitulo.Controls.Add(this.picTitulo);
+            this.pnlTitulo.Controls.Add(this.lblContenido);
+            this.pnlTitulo.Controls.Add(this.lblTitulo);
+            this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitulo.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnlTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitulo.Name = "pnlTitulo";
+            this.pnlTitulo.Size = new System.Drawing.Size(271, 45);
+            this.pnlTitulo.TabIndex = 0;
+            this.pnlTitulo.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlTitulo_Paint);
+            // 
+            // picTitulo
+            // 
+            this.picTitulo.BackColor = System.Drawing.Color.SeaGreen;
+            this.picTitulo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picTitulo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.picTitulo.ExtendedSizeMode = Sistema.ExtendedPictureBoxSizeMode.Fit;
+            this.picTitulo.Image = global::Sistema.Properties.Resources.invoice__2_;
+            this.picTitulo.Location = new System.Drawing.Point(0, 0);
+            this.picTitulo.Name = "picTitulo";
+            this.picTitulo.Padding = new System.Windows.Forms.Padding(4);
+            this.picTitulo.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            this.picTitulo.Size = new System.Drawing.Size(46, 45);
+            this.picTitulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picTitulo.TabIndex = 7;
+            this.picTitulo.TabStop = false;
+            // 
+            // lblContenido
+            // 
+            this.lblContenido.AutoSize = true;
+            this.lblContenido.Location = new System.Drawing.Point(51, 25);
+            this.lblContenido.Name = "lblContenido";
+            this.lblContenido.Size = new System.Drawing.Size(65, 13);
+            this.lblContenido.TabIndex = 2;
+            this.lblContenido.Text = "lblContenido";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(48, 4);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(71, 20);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "lblTitulo";
+            // 
+            // pnlSeparador
+            // 
+            this.pnlSeparador.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlSeparador.Location = new System.Drawing.Point(0, 45);
+            this.pnlSeparador.Name = "pnlSeparador";
+            this.pnlSeparador.Size = new System.Drawing.Size(271, 13);
+            this.pnlSeparador.TabIndex = 2;
+            // 
+            // pnlCuerpo
+            // 
+            this.pnlCuerpo.Controls.Add(this.lblLinea3);
+            this.pnlCuerpo.Controls.Add(this.lblLinea2);
+            this.pnlCuerpo.Controls.Add(this.lblLinea1);
+            this.pnlCuerpo.Controls.Add(this.picCuerpo);
+            this.pnlCuerpo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCuerpo.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnlCuerpo.Location = new System.Drawing.Point(0, 58);
+            this.pnlCuerpo.Name = "pnlCuerpo";
+            this.pnlCuerpo.Size = new System.Drawing.Size(271, 229);
+            this.pnlCuerpo.TabIndex = 3;
+            this.pnlCuerpo.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlCuerpo_Paint);
+            // 
+            // lblLinea3
+            // 
+            this.lblLinea3.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLinea3.Location = new System.Drawing.Point(21, 192);
+            this.lblLinea3.Name = "lblLinea3";
+            this.lblLinea3.Size = new System.Drawing.Size(49, 20);
+            this.lblLinea3.TabIndex = 3;
+            this.lblLinea3.Text = "lblLinea3";
+            // 
+            // lblLinea2
+            // 
+            this.lblLinea2.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLinea2.Location = new System.Drawing.Point(21, 169);
+            this.lblLinea2.Name = "lblLinea2";
+            this.lblLinea2.Size = new System.Drawing.Size(49, 20);
+            this.lblLinea2.TabIndex = 2;
+            this.lblLinea2.Text = "lblLinea2";
+            // 
+            // lblLinea1
+            // 
+            this.lblLinea1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLinea1.Location = new System.Drawing.Point(21, 146);
+            this.lblLinea1.Name = "lblLinea1";
+            this.lblLinea1.Size = new System.Drawing.Size(49, 20);
+            this.lblLinea1.TabIndex = 1;
+            this.lblLinea1.Text = "lblLinea1";
+            // 
+            // picCuerpo
+            // 
+            this.picCuerpo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picCuerpo.Image = global::Sistema.Properties.Resources.invoice__2_;
+            this.picCuerpo.Location = new System.Drawing.Point(54, 6);
+            this.picCuerpo.Name = "picCuerpo";
+            this.picCuerpo.Size = new System.Drawing.Size(155, 137);
+            this.picCuerpo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCuerpo.TabIndex = 0;
+            this.picCuerpo.TabStop = false;
+            // 
+            // Tarjeta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlCuerpo);
+            this.Controls.Add(this.pnlSeparador);
+            this.Controls.Add(this.pnlTitulo);
+            this.Name = "Tarjeta";
+            this.Size = new System.Drawing.Size(271, 287);
+            this.pnlTitulo.ResumeLayout(false);
+            this.pnlTitulo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTitulo)).EndInit();
+            this.pnlCuerpo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picCuerpo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTitulo;
+        private System.Windows.Forms.Label lblContenido;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Panel pnlSeparador;
+        private System.Windows.Forms.Panel pnlCuerpo;
+        private System.Windows.Forms.PictureBox picCuerpo;
+        private PctureBox picTitulo;
+        private System.Windows.Forms.Label lblLinea3;
+        private System.Windows.Forms.Label lblLinea2;
+        private System.Windows.Forms.Label lblLinea1;
+    }
+}
---
+++ Tarjeta.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema
+{
+    public partial class Tarjeta : UserControl
+    {
+        Color _colorBorde = Color.FromArgb(51, 182, 121);
+        int _ajusteMargen = 3;
+        Image _imagenTitulo = null;
+        Image _imagenCuerpo = null;
+        string _lineaTitulo = "Titulo";
+        string _lineaContenido = "Contenido";
+        string _linea1 = "Linea 1";
+        string _linea2 = "Linea 2";
+        string _linea3 = "Linea 3";
+        float _anchoLinea = 1;
+        Font _fuenteTitulo = new Font("Yu Gothic UI", 12, FontStyle.Bold);
+        Font _fuenteContenido = new Font("Yu Gothic UI", 9, FontStyle.Regular);
+        Font _fuenteLinea123 = new Font("Yu Gothic UI", 10, FontStyle.Bold);
+        Color _colorTitulo = Color.FromArgb(51, 182, 121);
+        Color _colorContenido = Color.FromArgb(51, 182, 121);
+        Color _colorLinea123 = Color.FromArgb(51, 182, 121);
+
+
+        private bool iKnowDaColor = true;
+
+        public float AnchoLinea
+        {
+            get { return _anchoLinea; }
+            set { _anchoLinea = value; Refresh(); }
+        }
+
+        public Color ColorLinea123
+        {
+            get { return _colorLinea123; }
+            set { _colorLinea123 = value; Refresh(); }
+        }
+
+        public Color ColorTitulo
+        {
+            get { return _colorTitulo; }
+            set { _colorTitulo = value; Refresh(); }
+        }
+
+        public Color ColorContenido
+        {
+            get { return _colorContenido; }
+            set { _colorContenido = value; Refresh(); }
+        }
+
+        public Font FuenteLinea123
+        {
+            get { return _fuenteLinea123; }
+            set { _fuenteLinea123 = value; Refresh(); }
+        }
+        public Font FuenteTitulo
+        {
+            get { return _fuenteTitulo; }
+            set { _fuenteTitulo = value; Refresh(); }
+        }
+
+        public Font FuenteContenido
+        {
+            get { return _fuenteContenido; }
+            set { _fuenteContenido = value; Refresh(); }
+        }
+
+        public string LineaTitulo
+        {
+            get { return _lineaTitulo; }
+            set { _lineaTitulo = value; Refresh(); }
+        }
+        public string LineaContenido
+        {
+            get { return _lineaContenido; }
+            set { _lineaContenido = value; Refresh(); }
+        }
+
+        public string Linea1
+        {
+            get { return _linea1; }
+            set { _linea1 = value; Refresh(); }
+        }
+
+        public string Linea2
+        {
+            get { return _linea2; }
+            set { _linea2 = value; Refresh(); }
+        }
+
+        public string Linea3
+        {
+            get { return _linea3; }
+            set { _linea3 = value; Refresh(); }
+        }
+
+        public Image ImagenCuerpo
+        {
+            get { return _imagenCuerpo; }
+            set { _imagenCuerpo = value; Refresh(); }
+        }
+        public Image ImagenTitulo
+        {
+            get { return _imagenTitulo; }
+            set { _imagenTitulo = value; Refresh(); }
+        }
+        public int AjusteMargen
+        {
+            get { return _ajusteMargen; }
+            set { _ajusteMargen = value; Refresh(); }
+        }
+
+        public Color ColorBorde
+        {
+            get { return _colorBorde; }
+            set { _colorBorde = value; Refresh(); }
+        }
+
+        public Tarjeta()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            // CenterTheBox(picCuerpo);
+            picCuerpo.Location = new Point((pnlCuerpo.Width / 2) - (picCuerpo.Width / 2), (pnlCuerpo.Height / 4) - (picCuerpo.Height / 4));
+
+            pnlCuerpo.Width = this.Width;
+            pnlCuerpo.Height = this.Height;
+
+            lblLinea1.Left = this.pnlCuerpo.Left + 5;
+            lblLinea1.Width = this.pnlCuerpo.Width - 15;
+            lblLinea1.Top = this.picCuerpo.Bottom + 5;
+
+            lblLinea2.Left = this.pnlCuerpo.Left + 5;
+            lblLinea2.Width = this.pnlCuerpo.Width - 15;
+            lblLinea2.Top = this.lblLinea1.Top + this.lblLinea1.Height + 2;
+
+            lblLinea3.Left = this.pnlCuerpo.Left + 5;
+            lblLinea3.Width = this.pnlCuerpo.Width - 15;
+            lblLinea3.Top = this.lblLinea2.Top + this.lblLinea2.Height + 2;
+        }
+
+        private void CenterTheBox(PictureBox picture)
+        {
+            picture.Size = picture.Size;
+            var left = (pnlCuerpo.ClientRectangle.Width - picture.ClientRectangle.Width) / 2;
+            var top = 1;  // (pnlCuerpo.ClientRectangle.Height - picture.ClientRectangle.Height) / (picture.Height / 3);
+            picture.Location = new System.Drawing.Point(pnlCuerpo.ClientRectangle.Location.X + left, pnlCuerpo.ClientRectangle.Location.Y + top);
+        }
+
+        private void pnlTitulo_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(new Pen(ColorBorde, AnchoLinea), 0, 0, this.pnlCuerpo.Width - 1, this.pnlTitulo.Height - 1);
+            picTitulo.Padding = new Padding(_ajusteMargen);
+            picTitulo.Image = ImagenTitulo;
+
+            lblTitulo.Text = LineaTitulo;
+            lblContenido.Text = LineaContenido;
+
+            lblTitulo.Font = FuenteTitulo;
+            lblContenido.Font = FuenteContenido;
+
+            lblTitulo.ForeColor = ColorTitulo;
+            lblContenido.ForeColor = ColorContenido;
+        }
+
+        private void pnlCuerpo_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(new Pen(ColorBorde, AnchoLinea), 0, 0, this.pnlCuerpo.Width - 1, this.pnlCuerpo.Height - 1);
+            picCuerpo.Image = ImagenCuerpo;
+
+            lblLinea1.Text = Linea1;
+            lblLinea2.Text = Linea2;
+            lblLinea3.Text = Linea3;
+
+            lblLinea1.Font = FuenteLinea123;
+            lblLinea2.Font = FuenteLinea123;
+            lblLinea3.Font = FuenteLinea123;
+
+            lblLinea1.ForeColor = ColorLinea123;
+            lblLinea2.ForeColor = ColorLinea123;
+            lblLinea3.ForeColor = ColorLinea123;
+        }
+    }
+}
